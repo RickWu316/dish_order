b6c106a20334492f131d522bdaea113b81233500
Rick Wu <rickwu316@gmail.com>
2021-03-28 22:30:21 +0800

    express init

---
+++ app.js
@@ -0,0 +1,33 @@
+// 載入 express 並建構應用程式伺服器
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars');
+const router = express.Router()
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const bcrypt = require('bcryptjs')
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+const PORT = process.env.PORT || 3000
+
+
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+
+
+
+// 設定 port 3000
+app.listen(PORT, () => {
+    console.log(`App is running on http://localhost:${PORT}`)
+})
+
+
+require('./routes')(app)
+
+module.exports = app
